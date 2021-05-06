98ae63e3f5040346f65c744558e0c08f1e9cab5f
rawdatalytics <contact.rawdatalytics@gmail.com>
2021-05-06 14:44:58 -0500

    Building logic to calculate 3 shifts hours

--- src/babysitter_kata/BabysitterKata_Dev.java
+++ src/babysitter_kata/BabysitterKata_Dev.java
@@ -1,5 +1,49 @@
 package babysitter_kata;
 
 public class BabysitterKata_Dev {
-
+	
+	//variables holding pay rates 
+	private int regularShiftHourlyPay = 12,
+			bedtimeShiftHourlyPay = 8,
+			midnightShiftHourlyPay = 16;
+	
+	//variables holding start and end time constraints for the work night
+	private int startHour = 5,
+			endHour = 4;
+	
+	
+	public int getRegularShiftHours() {
+		return 0;
+	}
+	
+	
+	public int getBedtimeShiftHours() {
+		return 0;
+	}
+	
+	public int getMidnightShiftHours() {
+		return 0;
+	}
+	
+	
+	// principle call to calculate the total pay for one night, given the start, end and bed times
+	public int getTotalPay(int startTime, int bedTime, int endTime) {
+		
+		int totalPay = 0;
+		
+	
+		int regularShiftHours = getRegularShiftHours(), 
+			bedtimeShiftHours = getBedtimeShiftHours(),
+			midnightShiftHours = getMidnightShiftHours();
+		
+		
+		int regularShiftPay = regularShiftHours * regularShiftHourlyPay,
+			bedtimeShiftPay = bedtimeShiftHours * bedtimeShiftHourlyPay,
+			midnightShiftPay = midnightShiftHours * midnightShiftHourlyPay;
+		
+		
+		totalPay = regularShiftPay + bedtimeShiftPay + midnightShiftPay;
+		
+		return totalPay;
+	}
 }
