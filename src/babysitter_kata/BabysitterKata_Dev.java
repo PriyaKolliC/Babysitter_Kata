1eb6a636abf90205e3dea824e4142fb725d4d0a7
rawdatalytics <contact.rawdatalytics@gmail.com>
2021-05-09 17:40:23 -0500

    rescaling time and adding test cases

--- src/babysitter_kata/BabysitterKata_Dev.java
+++ src/babysitter_kata/BabysitterKata_Dev.java
@@ -11,10 +11,18 @@ public class BabysitterKata_Dev {
 	private int startHour = 5,
 			endHour = 4;
 	
+	
+	//rescaling times from (5)17 to (4)28
+	public int rescaleTime(int time) {
+		if(time >= 5 && time <= 12)
+			return time + 12;
+		return time + 24;
+	}
+	
 	//start time cannot be before 5 pm
 	public boolean validateStartHour(int startTime) {
 		
-		if(startTime < startHour )
+		if( rescaleTime(startTime) < rescaleTime(startHour) )
 			return false;
 		
 		return true;
@@ -23,7 +31,7 @@ public class BabysitterKata_Dev {
 	//end time can be no later than 4 am 
 	public boolean validateEndHour(int endTime) {
 		
-		if(endTime > endHour)
+		if( rescaleTime(endTime) > rescaleTime(endHour) )
 			return false;
 		
 		return true;
@@ -33,7 +41,8 @@ public class BabysitterKata_Dev {
 	//bedtime cannot be: before start time and after end time
 	public boolean validateBedTimeHour(int bedTime, int startTime, int endTime) {
 
-		if (startTime <= bedTime && bedTime <= endTime) {
+		if ( rescaleTime(startTime) <= rescaleTime(bedTime) && 
+				rescaleTime(bedTime) <= rescaleTime(endTime) ){
 			return true;
 		} else
 			return false;
@@ -49,27 +58,26 @@ public class BabysitterKata_Dev {
 	//calculate regular shift hours
 	public int getRegularShiftHours(int startTime, int bedTime, int endTime) {
 		
-		if (bedTime >= 12) {
-			return 12 - startTime;
-		} else if (bedTime < 12) {
-			return bedTime - startTime;
+		if ( rescaleTime(bedTime) >= rescaleTime(12) ){
+			return rescaleTime(12) - rescaleTime(startTime);
+		} else if (rescaleTime(bedTime) < 24) {
+			return rescaleTime(bedTime) - rescaleTime(startTime);
 		} else
 			return 0;
-		
 	}
 	
 	//calculate bedtime shift hours
 	public int getBedtimeShiftHours(int startTime, int bedTime) {
-		if (startTime < 12) {
-			return 12 - bedTime;
+		if ( rescaleTime(startTime) < rescaleTime(12) ){
+			return rescaleTime(12) - rescaleTime(bedTime);
 		} else
 			return 0;
 	}
 	
 	//calculate midnight shift hours
 	public int getMidnightShiftHours(int endTime) {
-		if (endTime > 12) {
-			return endTime - 12;
+		if (rescaleTime(endTime) > rescaleTime(12) ){
+			return rescaleTime(endTime) - rescaleTime(12);
 		} else
 			return 0;
 	}
